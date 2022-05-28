08ec1ce2b524b04f1ec488f63fbce4fe3b758149
Aastha Chauhan <aastha4139@gmail.com>
2022-05-29 04:57:39 +0530

    Initial Commit

---
+++ script/index.js
@@ -0,0 +1,46 @@
+const form  = document.getElementById('displayForm');
+
+form.addEventListener('submit', (event) => {
+
+    var jT = document.getElementById('journeyType');
+    var jtID = jT.value;
+    // console.log(jtID);
+
+    var pr = document.getElementById('priceRange');
+    var prID = pr.value;
+    // console.log(prID);
+
+    if (jtID == "temp" && prID == "temp") {
+        document.getElementById('number1').style.display = "block";
+        document.getElementById('number2').style.display = "none";
+    }
+    else if (jtID == "temp" || prID == "temp") {
+        alert('Please Select on both Fields!');
+    }
+    else {
+
+        if (jtID == "1" && prID == "1") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=f20b3b26-5a81-4023-ac5a-dddf17b4225e&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (jtID == "1" && prID == "2") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=467a029d-b5b6-48f4-83bc-536abd375bdf&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (jtID == "1" && prID == "3") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=0702d417-f120-41e2-9232-16cd22ea7f8e&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (jtID == "2" && prID == "1") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=26139382-ce80-4a38-848f-66420243fcb4&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (jtID == "2" && prID == "2") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=4dd9e5bb-f92c-4d85-ac9a-c63a59704e2f&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (jtID == "2" && prID == "3") {
+            document.getElementById('iFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=444d7ea4-080b-49f7-8d22-6158a0968956&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+
+        document.getElementById('number1').style.display = "none";
+        document.getElementById('number2').style.display = "block";
+    }
+
+    event.preventDefault();
+});
---
+++ script/pldata.js
@@ -0,0 +1,45 @@
+const form  = document.getElementById('playForm');
+
+form.addEventListener('submit', (event) => {
+
+    var pl = document.getElementById('plot');
+    var plID = pl.value;
+
+    if (plID == "temp") {
+        document.getElementById('play1').style.display = "block";
+        document.getElementById('play2').style.display = "none";
+        document.getElementById('foot').style.position = "fixed";
+        document.getElementById('foot').style.bottom = "0";
+        document.getElementById('foot').style.left = "0";
+        document.getElementById('foot').style.width = "100%";
+    }
+    else {
+
+        if (plID == "1") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=ab4f3d7f-04b9-411a-9a17-7602f2402b3e&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (plID == "2") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=b2e4d077-590e-40cc-a013-ee29e4eeb42e&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (plID == "3") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=e118ee8d-b7f7-4170-ab15-e4d3f570706d&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (plID == "4") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=8689cc25-f37a-4d66-9eab-2a1f1dcdf111&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (plID == "5") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=25496d8a-8135-422f-b641-f3ccf01c1ea7&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+        else if (plID == "6") {
+            document.getElementById('playFrame').setAttribute('src',"https://app.powerbi.com/reportEmbed?reportId=096f6c5f-9ca6-4b3a-9fe0-f92bcfa62f17&autoAuth=true&ctid=242d5969-d4ad-4e47-9c9c-8af18d85fa02&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWluZGlhLWNlbnRyYWwtYS1wcmltYXJ5LXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9");
+        }
+
+        document.getElementById('play1').style.display = "none";
+        document.getElementById('play2').style.display = "block";
+
+        document.getElementById('foot').style.marginTop = "50px";
+        document.getElementById('foot').style.position = "relative";
+    }
+
+    event.preventDefault();
+});
